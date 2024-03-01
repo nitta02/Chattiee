659297099778bec11edd6cb0a1ad4a1881a877e9
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2024-03-01 22:44:32 +0600

    commit

--- lib/screens/pages/profile.dart
+++ lib/screens/pages/profile.dart
@@ -2,6 +2,7 @@
 import 'package:chattiee/model/chatuserModel.dart';
 import 'package:chattiee/screens/pages/login.dart';
 import 'package:chattiee/services/auth/constants.dart';
+import 'package:chattiee/services/checkUsers.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -17,93 +18,101 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   List dataList = [];
-
+  final globalKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Screen'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                auth.signOut().then((value) {
-                  Navigator.pop(context);
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile Screen'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  auth.signOut().then((value) {
+                    Navigator.pop(context);
 
-                  Navigator.pop(context);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ));
-                });
-              },
-              icon: const Icon(Icons.logout)),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Stack(children: [
-              CircleAvatar(
-                minRadius: 50,
-                child: Image.network(widget.userModel.image),
-              ),
-              Positioned(
-                bottom: 0,
-                top: 80,
-                right: 0,
-                left: 80,
-                child: MaterialButton(
-                  onPressed: () {},
-                  child: Icon(Icons.edit),
-                ),
-              ),
-            ]),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(widget.userModel.name),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              initialValue: widget.userModel.name,
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.person),
-                  label: const Text('Name'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  )),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              initialValue: widget.userModel.details,
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.info_outline),
-                  label: const Text('Details'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  )),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.edit),
-                    Text('Update'),
-                  ],
-                ))
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ));
+                  });
+                },
+                icon: const Icon(Icons.logout)),
           ],
+        ),
+        body: Form(
+          key: globalKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Stack(children: [
+                    CircleAvatar(
+                      minRadius: 50,
+                      child: Image.network(widget.userModel.image),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      top: 80,
+                      right: 0,
+                      left: 80,
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Icon(Icons.edit),
+                      ),
+                    ),
+                  ]),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(widget.userModel.name),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    initialValue: widget.userModel.name,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.person),
+                        label: const Text('Name'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    initialValue: widget.userModel.details,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.info_outline),
+                        label: const Text('Details'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.edit),
+                          Text('Update'),
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
