b14e64d756665cce32fa63d8746c2ef0916c65ae
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2024-03-04 21:16:26 +0600

    search funtion fully is added

--- lib/screens/pages/home.dart
+++ lib/screens/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:chattiee/model/chatuserModel.dart';
 import 'package:chattiee/screens/pages/profile.dart';
 import 'package:chattiee/services/auth/constants.dart';
@@ -16,7 +18,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<UserModel> dataList = [];
 
-  final List searchList = [];
+  final List<UserModel> searchList = [];
   bool isSearching = false;
 
   @override
@@ -28,107 +30,125 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: isSearching
-              ? TextFormField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Name/Email',
-                  ),
-                  onChanged: (value) {
-                    searchList.clear();
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: WillPopScope(
+        onWillPop: () {
+          if (isSearching) {
+            setState(() {
+              isSearching = !isSearching;
+            });
+            return Future.value(false);
+          } else {
+            return Future.value(true);
+          }
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: isSearching
+                  ? TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Name/Email',
+                      ),
+                      onChanged: (value) {
+                        searchList.clear();
 
-                    for (var element in dataList) {
-                      if (element.name
-                              .toLowerCase()
-                              .contains(value.toLowerCase()) &&
-                          element.email
-                              .toLowerCase()
-                              .contains(value.toLowerCase())) {
-                        searchList.add(value);
-                      }
+                        for (var element in dataList) {
+                          if (element.name
+                                  .toLowerCase()
+                                  .contains(value.toLowerCase()) ||
+                              element.email
+                                  .toLowerCase()
+                                  .contains(value.toLowerCase())) {
+                            searchList.add(element);
+                          }
+                          setState(() {
+                            searchList;
+                          });
+                        }
+                      },
+                    )
+                  : const Text(
+                      'Chattiee',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {
                       setState(() {
-                        searchList;
+                        isSearching = !isSearching;
                       });
-                    }
-                  },
-                )
-              : Text(
-                  'Chattiee',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.home),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isSearching = !isSearching;
-                  });
-                },
-                icon: Icon(isSearching ? CupertinoIcons.clear : Icons.search)),
-            IconButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProfileScreen(
-                          userModel: userModel,
-                        ),
-                      ));
-                },
-                icon: Icon(CupertinoIcons.person)),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add_comment),
-        ),
-        body: StreamBuilder(
-          stream: firebaseFirestore
-              .collection('users')
-              .where('id', isNotEqualTo: user.uid)
-              .snapshots(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-              case ConnectionState.none:
-                const Center(
-                  child: CircularProgressIndicator(),
-                );
+                    },
+                    icon: Icon(
+                        isSearching ? CupertinoIcons.clear : Icons.search)),
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfileScreen(
+                              userModel: userModel,
+                            ),
+                          ));
+                    },
+                    icon: const Icon(CupertinoIcons.person)),
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.add_comment),
+            ),
+            body: StreamBuilder(
+              stream: firebaseFirestore
+                  .collection('users')
+                  .where('id', isNotEqualTo: user.uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    const Center(
+                      child: CircularProgressIndicator(),
+                    );
 
-              case ConnectionState.active:
-              case ConnectionState.done:
-                if (snapshot.hasData) {
-                  final data = snapshot.data?.docs;
-                  dataList =
-                      data?.map((e) => UserModel.fromJson(e.data())).toList() ??
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    if (snapshot.hasData) {
+                      final data = snapshot.data?.docs;
+                      dataList = data
+                              ?.map((e) => UserModel.fromJson(e.data()))
+                              .toList() ??
                           [];
 
-                  if (dataList.isNotEmpty) {
-                    return ListView.builder(
-                      itemCount:
-                          isSearching ? searchList.length : dataList.length,
-                      physics: const BouncingScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return UserWidget(
-                          user:
-                              isSearching ? searchList[index] : dataList[index],
+                      if (dataList.isNotEmpty) {
+                        return ListView.builder(
+                          itemCount:
+                              isSearching ? searchList.length : dataList.length,
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return UserWidget(
+                              user: isSearching
+                                  ? searchList[index]
+                                  : dataList[index],
+                            );
+                          },
                         );
-                      },
-                    );
-                  }
+                      }
+                    }
                 }
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            )),
+      ),
+    );
   }
 }
