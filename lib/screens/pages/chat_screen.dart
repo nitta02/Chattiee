be94c6777ea21da5c490d5679e311e81877fdd25
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2024-03-23 20:16:36 +0600

    remove emoji package

--- lib/screens/pages/chat_screen.dart
+++ lib/screens/pages/chat_screen.dart
@@ -8,7 +8,6 @@ import 'package:chattiee/model/messageModel.dart';
 import 'package:chattiee/services/auth/constants.dart';
 import 'package:chattiee/services/user_Functions.dart';
 import 'package:chattiee/widgets/messages.dart';
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -98,34 +97,6 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                 ),
                 userMessageInput(),
-                SizedBox(
-                    height: 150,
-                    child: EmojiPicker(
-                      // onEmojiSelected: (
-                      //   Category category, Emoji emoji) {
-                      //   // Do something when emoji is tapped (optional)
-                      // },
-                      // onBackspacePressed: () {
-                      //   // Do something when the user taps the backspace button (optional)
-                      //   // Set it to null to hide the Backspace-Button
-                      // },
-                      textEditingController:
-                          emojiController, // pass here the same [TextEditingController] that is connected to your input field, usually a [TextFormField]
-                      config: Config(
-                        height: 256,
-                        // bgColor: const Color(0xFFF2F2F2),
-                        // checkPlatformCompatibility: true,
-                        emojiViewConfig: EmojiViewConfig(
-                          // Issue: https://github.com/flutter/flutter/issues/28894
-                          emojiSizeMax: 28 * (Platform.isIOS ? 1.20 : 1.0),
-                        ),
-                        // swapCategoryAndBottomBar: false,
-                        // skinToneConfig: const SkinToneConfig(),
-                        // categoryViewConfig: const CategoryViewConfig(),
-                        // bottomActionBarConfig: const BottomActionBarConfig(),
-                        // searchViewConfig: const SearchViewConfig(),
-                      ),
-                    ))
               ],
             ),
           ),
